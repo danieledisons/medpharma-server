726a3e3d24268943e5890645b3a269fa098ba130
danieledison <kanalt2700@gmail.com>
2024-05-25 17:58:30 +0100

    added cors

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const admin = require("firebase-admin");
+const cors = require("cors"); // Import cors
 const {
   uploadProcessedData,
   initializeFirebaseApp,
@@ -12,7 +13,14 @@ require("dotenv").config();
 const app = express();
 app.use(bodyParser.json());
 
+// Enable CORS with default options (allow all origins)
+app.use(cors());
+
+// OR Enable CORS for specific origin (for example, for your local development environment)
+// app.use(cors({ origin: 'http://localhost:3000' }));
+
 initializeFirebaseApp();
+
 function generateRandomString(length) {
   const characters =
     "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
@@ -33,8 +41,8 @@ app.post("/add-patient", async (req, res) => {
     const { name, gender, email, bloodpressure, weight } = req.body;
     const patientData = req.body;
 
-    if ((!name || !gender || !email || !bloodpressure, !weight)) {
-      res.status(404).json({
+    if (!name || !gender || !email || !bloodpressure || !weight) {
+      return res.status(404).json({
         error:
           "Invalid Request, Make sure to add name, gender, email, bloodpressure, weight",
       });
@@ -51,10 +59,12 @@ app.post("/add-patient", async (req, res) => {
 app.post("/login", async (req, res) => {
   const { username, password, role } = req.body;
   if (!username || !password) {
-    res.status(403).send({ message: "Username & Password are required" });
+    return res
+      .status(403)
+      .send({ message: "Username & Password are required" });
   }
   if (!role) {
-    res
+    return res
       .status(403)
       .send({ message: "Role is required, choose between Officer or Patient" });
   }
@@ -63,35 +73,33 @@ app.post("/login", async (req, res) => {
 // Route to create a new consultation
 app.post("/add-consultation", async (req, res) => {
   try {
-    if (
-      !req.body.patientLastName ||
-      !req.body.patientFirstName ||
-      !req.body.consultationDate ||
-      !req.body.consultationNotes ||
-      !req.body.medicalCondition
-    ) {
-      res.status(403).send({
-        message:
-          "Fill all forms using this format: patientFirstName, patientLastName, consultationDate,consultationNotes, medicalCondition",
-      });
-    }
-    const consultationData = req.body;
     const {
       patientFirstName,
       patientLastName,
       consultationDate,
-      consultationType = "General",
       consultationNotes,
       medicalCondition,
-    } = consultationData;
+    } = req.body;
 
-    // Generate a unique ID for the consultation document
+    if (
+      !patientFirstName ||
+      !patientLastName ||
+      !consultationDate ||
+      !consultationNotes ||
+      !medicalCondition
+    ) {
+      return res.status(403).send({
+        message:
+          "Fill all forms using this format: patientFirstName, patientLastName, consultationDate,consultationNotes, medicalCondition",
+      });
+    }
+
+    const consultationData = req.body;
     const consultationId = admin
       .firestore()
       .collection("consultations")
       .doc().id;
 
-    // Upload consultation data to Firestore with the generated ID
     await uploadProcessedData(
       "consultations",
       consultationId,
@@ -107,21 +115,19 @@ app.post("/add-consultation", async (req, res) => {
 // Route to filter consultations based on different criteria
 app.get("/filter-consultations", async (req, res) => {
   try {
-    // Extract filter criteria from query parameters
     const { patientFirstName, patientLastName, consultationDate } = req.query;
-    console.log(req.query);
 
-    // Call the filterConsultations function with the extracted criteria
     const filteredConsultations = await searchConsultations({
       patientFirstName,
       patientLastName,
       consultationDate,
     });
+
     const results = filteredConsultations.docs.map((doc) => ({
       id: doc.id,
       data: doc.data(),
     }));
-    // console.log(results);
+
     res.status(200).send(results);
   } catch (error) {
     console.error("Error filtering consultations: ", error);
